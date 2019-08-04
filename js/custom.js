153f119739ad1f13fec904580747f42cccec37bb
Mateus Fernandes <mateusfernandes12@live.com>
2019-08-04 10:25:16 -0300

    fffgfdgl

--- js/custom.js
+++ js/custom.js
@@ -90,14 +90,14 @@ $(document).ready(function() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1
         }
       }
